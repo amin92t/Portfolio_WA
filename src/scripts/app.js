1cde8be135f389bef1a830de4c59d097422c81cd
Amin Ataei <amin.ataei92@gmail.com>
2023-03-01 13:20:21 +0330

    complete home section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,9 @@
 const navBtn = document.querySelector('.nav__btn')
 const menu = document.querySelector('.menu')
+const coverbg = document.querySelector('.cover')
 
 navBtn.addEventListener('click', () => {
     navBtn.classList.toggle('nav__btn--active')
     menu.classList.toggle('menu--active')
+    coverbg.classList.toggle('cover--active')
 })
