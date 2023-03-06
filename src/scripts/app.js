ac672568f71b9e2371661de6ceed3bf84b1da510
Amin Ataei <amin.ataei92@gmail.com>
2023-03-06 16:50:27 +0330

    complete portfolio section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,6 +3,8 @@ const menu = document.querySelector('.menu')
 const coverbg = document.querySelector('.cover')
 
 const resumeListItems = document.querySelectorAll('.resume__list-item')
+const portfolioListItems = document.querySelectorAll('.portfolio__list-item')
+
 
 navBtn.addEventListener('click', () => {
     navBtn.classList.toggle('nav__btn--active')
@@ -20,3 +22,36 @@ resumeListItems.forEach(item => {
     })
 })
 
+
+portfolioListItems.forEach(item => {
+    item.addEventListener('click', () => {
+        document.querySelector('.portfolio__slider--show').classList.remove('portfolio__slider--show')
+        document.querySelector('.portfolio__list-item--active').classList.remove('portfolio__list-item--active')
+        item.classList.add('portfolio__list-item--active')
+        let sliderId = item.getAttribute('data-content-id')
+        document.querySelector(sliderId).classList.add('portfolio__slider--show')
+    })
+})
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        DynamicBullets: true,
+
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        600: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3
+        }
+    }
+});
