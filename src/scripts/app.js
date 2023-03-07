52e4f683e84ec1738f34ee7be27d6bc9f16bbd08
Amin Ataei <amin.ataei92@gmail.com>
2023-03-07 10:12:36 +0330

    refactor js file

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -12,26 +12,51 @@ navBtn.addEventListener('click', () => {
     coverbg.classList.toggle('cover--active')
 })
 
-resumeListItems.forEach(item => {
-    item.addEventListener('click', () => {
-        document.querySelector('.resume__content--show').classList.remove('resume__content--show')
-        document.querySelector('.resume__list-item--active').classList.remove('resume__list-item--active')
-        item.classList.add('resume__list-item--active')
-        let resumeContentId = item.getAttribute("data-content-id")
-        document.querySelector(resumeContentId).classList.add('resume__content--show')
+
+// =========================================  listItem Function
+
+function tabChanges(listItems, showClass, activeClass) {
+    listItems.forEach(item => {
+        item.addEventListener('click', () => {
+            document.querySelector(`.${showClass}`).classList.remove(showClass)
+            document.querySelector(`.${activeClass}`).classList.remove(activeClass)
+            item.classList.add(activeClass)
+            let resumeContentId = item.getAttribute("data-content-id")
+            document.querySelector(resumeContentId).classList.add(showClass)
+        })
     })
-})
+}
+
+tabChanges(resumeListItems, "resume__content--show", "resume__list-item--active")
+tabChanges(portfolioListItems, "portfolio__slider--show", "portfolio__list-item--active")
+
+// resumeListItems.forEach(item => {
+//     item.addEventListener('click', () => {
+//         document.querySelector('.resume__content--show').classList.remove('resume__content--show')
+//         document.querySelector('.resume__list-item--active').classList.remove('resume__list-item--active')
+//         item.classList.add('resume__list-item--active')
+//         let resumeContentId = item.getAttribute("data-content-id")
+//         document.querySelector(resumeContentId).classList.add('resume__content--show')
+//     })
+// })
 
 
-portfolioListItems.forEach(item => {
-    item.addEventListener('click', () => {
-        document.querySelector('.portfolio__slider--show').classList.remove('portfolio__slider--show')
-        document.querySelector('.portfolio__list-item--active').classList.remove('portfolio__list-item--active')
-        item.classList.add('portfolio__list-item--active')
-        let sliderId = item.getAttribute('data-content-id')
-        document.querySelector(sliderId).classList.add('portfolio__slider--show')
-    })
-})
+// portfolioListItems.forEach(item => {
+//     item.addEventListener('click', () => {
+//         document.querySelector('.portfolio__slider--show').classList.remove('portfolio__slider--show')
+//         document.querySelector('.portfolio__list-item--active').classList.remove('portfolio__list-item--active')
+//         item.classList.add('portfolio__list-item--active')
+//         let sliderId = item.getAttribute('data-content-id')
+//         document.querySelector(sliderId).classList.add('portfolio__slider--show')
+//     })
+// })
+
+
+
+
+
+
+// =========================================  Swiper Js
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
